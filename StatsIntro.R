4989183497a7bd9b390b74196e5e6a37ea56fc8e
Ryan Womack <rwomack@scarletmail.rutgers.edu>
2019-05-14 18:00:46 -0400

    added A/B subroutine

--- StatsIntro.R
+++ StatsIntro.R
@@ -11,6 +11,7 @@ install.packages("tabplot", dependencies=TRUE)
 install.packages("lattice", dependencies=TRUE)
 install.packages("rstan", dependencies=TRUE)
 install.packages("ggplot2", dependencies = TRUE)
+install.packages("truncnorm", dependencies = TRUE)
 
 # Card draw
 pclub<-0.25
@@ -71,11 +72,94 @@ mydiff<-(pricedata$Barry.Price - pricedata$Mary.Price)
 mydiff
 sign(mydiff)
 
-#t-Test
+# t-Test
 t.test(mydiff)
 
-#2 sample t-test
+# 2 sample t-test
 t.test(pricedata$Barry.Price, pricedata$Mary.Price, paired=TRUE)
+
+# A/B test simulation
+
+# see appendix for how this data was generated
+
+# two samples of 100,000 observations have been created
+# suppose the number represents the dollar amount sold on a website under two different designs (A,B)
+# can you tell if there is a significant difference in the average price between A and B?
+
+# read in data
+sample_A_full <- read.csv("groupA.csv")
+sample_B_full <- read.csv("groupB.csv")
+
+sample_A_full <- sample_A_full$x
+sample_B_full <- sample_B_full$x
+
+# create small, medium, and large extracts
+
+sample_A_small <- sample(sample_A_full, 10)
+sample_B_small <- sample(sample_B_full, 10)
+
+sample_A_medium <- sample(sample_A_full, 100)
+sample_B_medium <- sample(sample_B_full, 100)
+
+sample_A_large <- sample(sample_A_full, 1000)
+sample_B_large <- sample(sample_B_full, 1000)
+
+# t-test for small
+t.test(sample_A_small, sample_B_small, paired=FALSE)
+
+# t-test for medium
+t.test(sample_A_medium, sample_B_medium, paired=FALSE)
+
+# t-test for large
+t.test(sample_A_large, sample_B_large, paired=FALSE)
+
+# t-test for full
+t.test(sample_A_full, sample_B_full, paired=FALSE)
+
+# try this again as a function
+
+my_t_test <- function()
+{
+
+sample_A_full <- read.csv("groupA.csv")
+sample_B_full <- read.csv("groupB.csv")
+
+sample_A_full <- sample_A_full$x
+sample_B_full <- sample_B_full$x
+
+# create small, medium, and large extracts
+
+sample_A_small <- sample(sample_A_full, 10)
+sample_B_small <- sample(sample_B_full, 10)
+
+sample_A_medium <- sample(sample_A_full, 100)
+sample_B_medium <- sample(sample_B_full, 100)
+
+sample_A_large <- sample(sample_A_full, 1000)
+sample_B_large <- sample(sample_B_full, 1000)
+
+# t-test for small
+small <- t.test(sample_A_small, sample_B_small, paired=FALSE)
+
+# t-test for medium
+medium <- t.test(sample_A_medium, sample_B_medium, paired=FALSE)
+
+# t-test for large
+large <- t.test(sample_A_large, sample_B_large, paired=FALSE)
+
+# t-test for full
+full <- t.test(sample_A_full, sample_B_full, paired=FALSE)
+
+output<- list("small", small, "medium", medium, "large", large, "full", full)
+return(as.data.frame(do.call(cbind, output)))
+}
+
+# now try it out - each time it will generate different random samples
+
+my_t_test()
+
+
+
 
 # Cell Data
 celldata<-read.csv("cellphoneservice.csv")
@@ -87,7 +171,7 @@ describe(celldata)
 library(tabplot)
 tableplot(celldata)
 
-#linear regression on Wind Speed
+# linear regression on Wind Speed
 library(lattice)
 
 cor(celldata$Bad_Calls, celldata$Wind_Speed)
@@ -98,7 +182,7 @@ summary(my_reg_wind)
 plot(my_reg_wind)
 xyplot(celldata$Bad_Calls~celldata$Wind_Speed, type=c("p","r"))
 
-#linear regression on Pressure
+# linear regression on Pressure
 
 cor(celldata$Bad_Calls, celldata$Pressure)
 plot(celldata$Bad_Calls~celldata$Pressure)
@@ -109,7 +193,7 @@ my_reg_pressure<-lm(Bad_Calls ~ Pressure, data=celldata)
 summary(my_reg_pressure)
 plot(my_reg_pressure)
 
-#multivariate regression
+# multivariate regression
 
 cor(celldata$Pressure, celldata$Wind_Speed)
 splom(celldata)
@@ -127,22 +211,22 @@ plot(my_reg_multi)
 library(rstan)
 rstan_options(auto_write = TRUE)
 
-#plot beta densities
+# plot beta densities
 
 a=1
 b=1
 p=seq(0,1,.002)
 plot(p,dbeta(p,a,b),typ='l')
 
-#Brute force approach to the binomial
+# Brute force approach to the binomial
 
 # y~Binomal(p,N)
 # p~beta(a,b)
 
 # find p|y
 
-y=78
-n=100
+y=6
+n=10
 a=1
 b=1
 
@@ -180,3 +264,24 @@ plot(model)
 
 # and another
 # https://alexanderetz.com/2015/07/25/understanding-bayes-updating-priors-via-the-likelihood/
+
+
+# appendix on generating data
+#   this function was used to generate two random data samples, examples of which are on the github site
+#   try different numbers for n, mean, and sd
+#   for the first in-class example, we will use n=100000,mean=100,sd=10
+#   note we impose a lower bound (0) and upper bound (1000) on the numbers
+#   therefore the distribution is a truncated normal
+
+library(truncnorm)
+
+myexperiment<-function(n,mean,sd)
+{
+  rtruncnorm(n,0,1000,mean,sd)
+}
+
+groupA<-myexperiment(100000,100,30)
+groupB<-myexperiment(100000,105,30)
+
+write.csv(groupA, file="groupA.csv")
+write.csv(groupB, file="groupB.csv")
